5d57bba064f5eec9a12c9b2fac37536ed6efe878
Paula-Wang <nimopaula8@gmail.com>
2026-01-25 16:49:47 +0300

    Deploy Flutter web UI to server root

--- cognitive_load_butler_server/web/flutter_service_worker.js
+++ cognitive_load_butler_server/web/flutter_service_worker.js
@@ -3,20 +3,18 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "86a476337fb3063fb4a9a12ce5b6764c",
+const RESOURCES = {"flutter_bootstrap.js": "9c66d323c0c1d7d973a23f43d6036ec4",
 "version.json": "9aaea543ed349f52bda661631e82542c",
-"index.html": "59a2da110d773fd18453a30cf78ceeaf",
-"/": "59a2da110d773fd18453a30cf78ceeaf",
+"index.html": "d66943170525a5dd131adea06d4d2be2",
+"/": "d66943170525a5dd131adea06d4d2be2",
 "main.dart.js": "d8dd022eb76ea55bf61f219621cc801d",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.mjs": "2964e933c1eadf194ba9fa35fe520ee2",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "2ef28209dd661766ff1b5bd560bf9643",
-"main.dart.wasm": "0b57c7cbb0ab1da0b6b4094c511554f0",
 "assets/AssetManifest.json": "14966ac89c0666ce48d8df962dadb3f4",
 "assets/NOTICES": "7b761e16ecb7c61fa8b20eb1f341b7d4",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
@@ -43,8 +41,6 @@ const RESOURCES = {"flutter_bootstrap.js": "86a476337fb3063fb4a9a12ce5b6764c",
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
-"main.dart.wasm",
-"main.dart.mjs",
 "index.html",
 "flutter_bootstrap.js",
 "assets/AssetManifest.bin.json",
